ce77981f1325f09ef864a55b7882df6caa1d871a
Phillip Kravtsov <phillip@kravtsov.net>
2018-10-27 13:59:52 -0700

    split works now lol

--- homework/6/split.py
+++ homework/6/split.py
@@ -13,32 +13,36 @@ import random
 DATA_PATH = sys.argv[1]
 proportion = 0.8
 random.seed(11235812)
+classes = ['deer', 'sheep', 'hartebeest']
 
 
 for path in glob(os.path.join(DATA_PATH, '*')):
-    if not os.path.isdir(path):
+    class_ = path.split('/')[-1].lower()
+    if not os.path.isdir(path) or class_ not in classes:
         continue
     print(path)
+    class_ = path.split('/')[-1].lower()
+    print(class_)
     imgs = glob(os.path.join(path, '*.jpg'))
     imgs.sort()
     random.shuffle(imgs)
     try:
-        os.mkdir(os.path.join(path, 'train'))
+       os.makedirs(os.path.join(DATA_PATH, 'train', class_))
     except FileExistsError:
         pass
     try:
-        os.mkdir(os.path.join(path, 'val'))
+        os.makedirs(os.path.join(DATA_PATH, 'val', class_))
     except FileExistsError:
         pass
     split_idx = int(0.8 * len(imgs))
     train_imgs = imgs[:split_idx]
     val_imgs = imgs[split_idx:]
     for img_path in train_imgs:
-        target_path = os.path.join(path, 'train', img_path.split('/')[-1])
+        target_path = os.path.join(DATA_PATH, 'train' , class_,  img_path.split('/')[-1])
         os.rename(img_path, target_path)
         print(img_path, "  -->  ", target_path)
     for img_path in val_imgs:
-        target_path = os.path.join(path, 'val', img_path.split('/')[-1])
+        target_path = os.path.join(DATA_PATH, 'val' , class_,  img_path.split('/')[-1])
         os.rename(img_path, target_path)
         print(img_path, "  -->  ", target_path)
 
